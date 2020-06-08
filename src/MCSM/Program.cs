0fcf100fd1ab80fc292899b5c91008b351e019d7
minecraftplayer456 <no_email>
2020-06-08 09:32:25 +0200

    Add solution

---
+++ src/MCSM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

52213515ebab4202730baa0494f516977285ef67
minecraftplayer456 <no_email>
2020-06-20 12:46:12 +0200

    Remove all classes

--- src/MCSM/App.cs
+++ src/MCSM/App.cs
@@ -1,34 +1,9 @@
-﻿using MCSM.Views;
-using Serilog;
-using Serilog.Core;
-using Serilog.Events;
-using Terminal.Gui;
-using Constants = MCSM.Util.Constants;
-
-namespace MCSM
+﻿namespace MCSM
 {
     public class App
     {
-        private void Start()
-        {
-            var consoleLogLevelSwitch = new LoggingLevelSwitch(Constants.DefaultLogLevel);
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Is(Constants.DefaultLogLevel)
-                .WriteTo.Console(levelSwitch: consoleLogLevelSwitch)
-                .WriteTo.File("logs/latest.txt")
-                .CreateLogger();
-
-            Log.Information(Constants.MCSMShortNameVersion);
-
-            consoleLogLevelSwitch.MinimumLevel = LogEventLevel.Fatal;
-            Application.Run<RootView>();
-        }
-
         public static void Main(string[] args)
         {
-            var app = new App();
-            app.Start();
         }
     }
 }
